9d03e2214919fc08352daa2ec3cc6539ca3895cf
jodacame <jodacame@gmail.com>
2021-07-07 18:36:12 -0500

    Fixed bug

--- test.js
+++ test.js
@@ -1,9 +1,34 @@
 const gmeta = require("./gmeta");
+console.log("GMeta: Test!");
+
+// Using callback
+
+gmeta("https://github.com", function (err, data) {
+  console.log("[CALLBACK] GMeta: Test from URL!", data);
+});
+
+gmeta(
+  '<title>GitHub</title><meta name="description" content="GitHub is where people build software. More than 28 million people use GitHub to discover, fork, and contribute to over 85 million projects.">',
+  function (err, data) {
+    console.log("[CALLBACK] GMeta: Test from HTML!", data);
+  },
+  true
+);
 
 (async () => {
   try {
-    const result = await gmeta("https://www.byeindonesia.com/");
+    const result = await gmeta("https://github.com");
     console.log("[PROMISE] GMeta: Test from URL!", result);
+  } catch (err) {
+    console.log(err);
+  }
+
+  try {
+    const result = await gmeta(
+      '<title>GitHub</title><meta name="description" content="GitHub is where people build software. More than 28 million people use GitHub to discover, fork, and contribute to over 85 million projects.">',
+      true
+    );
+    console.log("[PROMISE] GMeta: Test from HTML!", result);
   } catch (err) {
     console.log(err);
   }
